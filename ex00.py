f8dd829a866a278594c7cf816c072168e680ba97
LinaPeritt <148325201+LinaPeritt@users.noreply.github.com>
2024-05-13 12:21:32 +0200

    ajout ex 0

---
+++ ex00.py
@@ -0,0 +1,7 @@
+km = 1.60934
+def convert():
+    miles = float(input("Entrez la valeur en miles: "))
+    conversion = miles * km
+    print(f"Une fois convertit en km, cela fait {conversion} km.")
+
+convert()
